bd32ccfe5a8d2baf03d9fdcda374a718e981a86a
Sargates <nick@WSL00-NG>
2023-11-02 22:21:30 -0500

    Added alias to list open ports

--- profile.zsh
+++ profile.zsh
@@ -13,14 +13,16 @@ alias cdzsh='cd ~/.zshconfig'
 alias haconnect='ssh hassio@homeassistant.local' 		# Connect to local home assisstant server (if exists)
 alias listapt='apt list --installed'					# List installed apt packages (optionally use with grep)
 alias listjava='apt search openjdk-.+-'					# Search installable JDK's because I can never remember the formatting
-alias clip="cut -c 1-`tput cols`"						# Use with pipe to clip output to terminal size to prevent line wrapping
-alias clgrep="clip|grep"								# Use with pipe to have clipped grep output. Buggy; Doesn't match patterns to what was clipped
-alias ncgrep="grep --color=never"						# Use with pipe to have clipped grep output. Buggy; Doesn't match patterns to what was clipped
-alias l="ls -lah --color=always"						# Preserves coloring when piping to grep with --color=never flag i.e. `l | grep --color=never {PATTERN}`
+
 
 alias zshcfg="$EDITOR ~/.zshconfig"						# Open zsh config in VSCode
 alias mcd='() { md $1 && cd $_ }'						# Used to create and cd to a new directory
 alias killssh='kill `pgrep ssh-agent`' 					# Used to reset ssh-agent
+alias clip="cut -c 1-`tput cols`"						# Use with pipe to clip output to terminal size to prevent line wrapping
+alias clgrep="clip|grep"								# Use with pipe to have clipped grep output. Buggy; Doesn't match patterns to what was clipped
+alias ncgrep="grep --color=never"						# Use with pipe to have clipped grep output. Buggy; Doesn't match patterns to what was clipped
+alias l="ls -lah --color=always"						# Preserves coloring when piping to grep with --color=never flag i.e. `l | grep --color=never {PATTERN}`
+alias listports="sudo lsof -i -P -n | grep LISTEN"		# Used to list open ports
 
 
 
