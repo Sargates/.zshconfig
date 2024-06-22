828bda6867db71c6c17329abd46c1a187fd893b4
Sargates <nick@WSL00-NG>
2024-06-22 13:13:02 -0500

    Updated directory structure; directory names, deleted unused files

--- scripts/symlinks.zsh
+++ scripts/symlinks.zsh
@@ -1,8 +1,23 @@
 # This file ensures symlinks in `~` exist and point to the files in `configs`
 
-source "$ZSHCFG/scripts/utils/saveAndLink.zsh" # This script uses functions defined in `utils.zsh`
+# source "$ZSHCFG/scripts/utils/saveAndLink.zsh" # This script uses functions defined in `utils.zsh`
 
-saveAndLink ".zshrc" "$HOME"
-for config in $ZSHCFG/configs/*; do
-	saveAndLink "configs/${config:t}" "$HOME"
+# Not needed to re-link .zshrc because this script wont get 
+for file in .zshrc .tmux.conf .tmux.conf.local .gitconfig; do
+	[ $needsUpdate ] && break # exit to update symlinks
+
+	local filepath=$HOME/$file
+	[ ! -h $filepath ] && needsUpdate=1 												# if file is not a symlink, overwrite with symlink
+	[ -z $needsUpdate ] && [[ $(readlink $filepath) != "$ZSHCFG/"* ]] && needsUpdate=1	# if file is symlink and symlink does not point somewhere in $ZSHCFG, overwrite with proper symlink
 done
+
+if [ $needsUpdate ]; then
+	echo "Updating Symlinks"
+	
+	# List all files in config directory, run ln on all results
+	# Wrapping command in () opens subshell, cd into directory 
+	(cd $ZSHCFG/config && ls -A1) | xargs -l zsh -c 'ln -sf $ZSHCFG/config/$0 ~/$0'
+	ln -sf $ZSHCFG/.zshrc ~/.zshrc
+fi
+
+
