c1e81ad081977e48a4eb0994a79bb87ccac63952
Sargates <nick@WSL00-NG>
2024-06-24 12:48:14 -0500

    Updated `symlinks.zsh` to make a backup of any files it overwrites. Also works for the installation script because `install.bash` calls `symlinks.zsh` directly.

--- scripts/symlinks.zsh
+++ scripts/symlinks.zsh
@@ -12,9 +12,13 @@ done
 if [ $needsUpdate ]; then
 	echo "Updating Symlinks"
 	
-	# List all files in config directory, run ln on all results
-	# Wrapping command in () opens subshell, cd into directory 
-	(cd $ZDOTDIR/config && ls -A1) | xargs -l zsh -c 'ln -sf $ZDOTDIR/config/$0 ~/$0'
+	# List all files in config directory, pass to xargs to run command on each result
+	# Wrapping command in () opens subshell, cd into directory
+	(cd $ZDOTDIR/config && ls -A1) | xargs -l zsh -c 'cat $ZDOTDIR/config/$0 >> $ZDOTDIR/.cache/$0.old'	# Make backup of old file
+	(cd $ZDOTDIR/config && ls -A1) | xargs -l zsh -c 'ln -sf $ZDOTDIR/config/$0 ~/$0'					# Make symlink in $HOME
+	
+	# Do same with .zshrc, not in `config` so needs to be done separately
+	cat $ZDOTDIR/config/.zshrc >> $ZDOTDIR/.cache/.zshrc.old
 	ln -sf $ZDOTDIR/.zshrc ~/.zshrc
 fi
 
