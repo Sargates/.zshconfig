17de70cef541d67b29983bb14f792066c2502cc9
Sargates <nick@WSL00-NG>
2024-07-02 10:49:16 -0500

    Added `config.zsh` support for `wsl.zsh` and `linux.zsh`

--- scripts/platform/linux.zsh
+++ scripts/platform/linux.zsh
@@ -1,4 +1,4 @@
-if [[ $ISWSL ]]; then
+if (( $ISWSL )); then
 	echo "Should not load linux.zsh on wsl instance"
 	return
 fi
@@ -8,31 +8,34 @@ compinit
 
 
 alias desktop='~/Desktop'
-alias desk='desktop'
 alias ds='desktop'
-alias dl='~/Downloads'
-alias dev='~/dev'
-
-mkdir -p ~/dev/CS-Stuff
-
-builtin hash -d cs=~/dev/CS-Stuff
 builtin hash -d d=~/Desktop
+
+alias dl='~/Downloads'
 builtin hash -d dl=~/Downloads
 
+if (( ${+ZSH_CONFIG[create_cs]} )); then
+	mkdir -p ~/dev/CS-Stuff
+	builtin hash -d cs=~/dev/CS-Stuff
+	alias dev='~/dev'
+fi
 
 
-function cs-stuff() { # "function" keyword seems required here or terminal yells at you
-	BASEPATH=~cs
-	SUBPATH=$1
-	if [ -d "$BASEPATH/$SUBPATH" ]; then
-		cd $BASEPATH/$SUBPATH
-		return 0
-	fi
-	if [ -e "$BASEPATH/$SUBPATH" ]; then
-		echo $BASEPATH/$SUBPATH
-		return 0
-	fi
-	echo "Could not find file \"~cs/$SUBPATH\""
-	return 1
-}
-compdef "_directories -/ -W ~cs" cs-stuff
+
+
+#? I dont use this anymore, here just to look at
+# function cs-stuff() { # "function" keyword seems required here or terminal yells at you
+# 	BASEPATH=~cs
+# 	SUBPATH=$1
+# 	if [ -d "$BASEPATH/$SUBPATH" ]; then
+# 		cd $BASEPATH/$SUBPATH
+# 		return 0
+# 	fi
+# 	if [ -e "$BASEPATH/$SUBPATH" ]; then
+# 		echo $BASEPATH/$SUBPATH
+# 		return 0
+# 	fi
+# 	echo "Could not find file \"~cs/$SUBPATH\""
+# 	return 1
+# }
+# compdef "_directories -/ -W ~cs" cs-stuff
--- scripts/platform/wsl.zsh
+++ scripts/platform/wsl.zsh
@@ -1,19 +1,62 @@
-if [[ ! $ISWSL ]]; then
+if (( ! $ISWSL )); then
 	echo "Should not load wsl.zsh on non-wsl instance"
 	return
 fi
 
-# autoload -Uz compinit
-# compinit
+builtin hash -d w=/mnt/c/Users/$USER 								# Hash for Windows user directory
+alias desktop='~w/Desktop'; 	alias desk='desktop'				# Desktop Aliases
+alias ds='desktop'; 			builtin hash -d d=~w/Desktop		# Hash for Windows Desktop
+alias dl='~w/Downloads';		builtin hash -d dl=~w/Downloads		# Hash for Windows Downloads
+# alias obs='~w/Desktop/Obsidian'
 
-alias root='/mnt/c/'
-alias desktop='~w/Desktop'
-alias desk='desktop'
-alias ds='desktop'
-alias dl='~w/Downloads'
-# alias dev='/mnt/c/dev'
-alias dev='~/dev'
-alias obs='~w/Desktop/Obsidian'
+alias ld='~/Desktop'		# overrides GNU linker, I don't care
+alias ldl='~/Downloads'
+
+
+if (( ${+ZSH_CONFIG[create_cs]} )); then
+	mkdir -p ~/dev/CS-Stuff
+	builtin hash -d cs=~/dev/CS-Stuff
+	alias dev='~/dev'
+fi
+
+#* Just use `python.exe`, should be identical
+# alias winpy='psx python'
+# alias winpython='winpy'
+
+
+open() {
+	#? Potential change: Use the `:A` expansion selector. evaluates relative path traversals 
+	#? like `.` and `..` and expands them to an absolute path. Use this instead of the double check
+	# Might still have issue when absolute path is passed, seems to work
+	if [[ -e ${1:A} ]]; then 				# check absolute path
+		explorer.exe "`wslpath -w ${1:A}`"
+		return 0
+	fi
+	echo "No such file or directory: $1"
+	return 1
+}
+alias psx="powershell.exe" # Syntax: `psx start .` (opens current dir)
+alias cmd="cmd.exe"
+alias cmdx="cmd /C"
+
+# commented out because fuck mono
+# alias fixmono='sudo update-binfmts --disable cli' # required for Mono on WSL instance
+alias gaming='python ~cs/Python/utils/gigachad.py'
+export LIBGL_ALWAYS_SOFTWARE=1 # This being unset causes a segmentation fault in some cases, `glxinfo, glxgears` See: https://github.com/microsoft/wslg/issues/715#issuecomment-1419138688 for full thread
+
+# Execute dotnet program in powershell to leverage Windows
+alias dnr="dotnet.exe run"
+alias dnb="dotnet.exe build"
+
+
+# export DISPLAY=192.168.176.1:0.0 # garbage for xserver
+# export LIBGL_ALWAYS_INDIRECT=1
+
+
+winpwd() {
+	echo $(wslpath -m .)
+}
+
 
 # This MinGW cmake toolchain file can be found here: https://github.com/glfw/glfw/blob/master/CMake/x86_64-w64-mingw32.cmake
 function wincmake() {
@@ -27,47 +70,22 @@ function wincmake() {
 }
 alias mingwcmake='wincmake'
 
-alias ld='~/Desktop'		# overrides GNU linker, I don't care
-alias ldl='~/Downloads'
-alias ldesktop='ld'
-alias ldesk='ld'
 
-alias cmd="cmd.exe"
-alias cmdx="cmd /C"
-
-open() {
-	#? Potential change: Use the `:A` expansion selector. evaluates relative path traversals 
-	#? like `.` and `..` and expands them to an absolute path. Use this instead of the double check
-	# Might still have issue when absolute path is passed, seems to work
-	if [[ -e $1 ]]; then 				# check absolute path
-		explorer.exe "`wslpath -w $1`"
-		return 0
-	elif [[ -e $PWD/$1 ]]; then 		# check relative path
-		explorer.exe "`wslpath -w $PWD/$1`"
-		return 0
-	fi
-	echo "No such file or directory: $1"
-	return 1
-}
-alias psx="powershell.exe" # Syntax: `psx start .` (opens current dir)
-
-mkdir -p ~/dev/CS-Stuff
-builtin hash -d w=/mnt/c/Users/Nick
-builtin hash -d cs=~/dev/CS-Stuff
-builtin hash -d d=~w/Desktop
-builtin hash -d dl=~w/Downloads
-
-alias winpy='psx python'
-alias winpython='winpy'
-
-# commented out because fuck mono
-# alias fixmono='sudo update-binfmts --disable cli' # required for Mono on WSL instance
-alias gaming='python `cs-stuff Python/utils/gigachad.py`'
-export LIBGL_ALWAYS_SOFTWARE=1 # This being unset causes a segmentation fault in some cases, `glxinfo, glxgears` See: https://github.com/microsoft/wslg/issues/715#issuecomment-1419138688 for full thread
-
-# Execute dotnet program in powershell to leverage Windows
-alias dnr="psx dotnet run"
-alias dnb="psx dotnet build"
+# function cs-stuff() { # "function" keyword seems required here or terminal yells at you
+# 	BASEPATH=~cs
+# 	SUBPATH=$1
+# 	if [ -d "$BASEPATH/$SUBPATH" ]; then
+# 		cd $BASEPATH/$SUBPATH
+# 		return 0
+# 	fi
+# 	if [ -e "$BASEPATH/$SUBPATH" ]; then
+# 		echo $BASEPATH/$SUBPATH
+# 		return 0
+# 	fi
+# 	echo "Could not find file \"~cs/$SUBPATH\""
+# 	return 1
+# }
+# compdef "_directories -/ -W ~cs" cs-stuff
 
 #! `--mouse` flag breaks Windows Terminal selection inside of pagers (git log, man, etc.). See: https://github.com/microsoft/terminal/issues/9165#issuecomment-1398208221
 #! Holding shift down prevents this issue, but interaction still works the same (i.e. it will assume that you are selecting instead of deselecting because you are holding shift)
@@ -75,32 +93,6 @@ alias dnb="psx dotnet build"
 [[ "$LESS" != *--wheel-lines=* ]] && export LESS="$LESS --wheel-lines=2" 	# Change number of lines per scroll
 # [[ "$LESS" != *-N* ]] 	&& export LESS="$LESS -N" 							# Show line numbers in less by default
 #! disabled above because this messes with too many tools that use less
-
-# export DISPLAY=192.168.176.1:0.0 # garbage for xserver
-# export LIBGL_ALWAYS_INDIRECT=1
-
-
-winpwd() {
-	echo $(wslpath -m .)
-}
-
-
-function cs-stuff() { # "function" keyword seems required here or terminal yells at you
-	BASEPATH=~cs
-	SUBPATH=$1
-	if [ -d "$BASEPATH/$SUBPATH" ]; then
-		cd $BASEPATH/$SUBPATH
-		return 0
-	fi
-	if [ -e "$BASEPATH/$SUBPATH" ]; then
-		echo $BASEPATH/$SUBPATH
-		return 0
-	fi
-	echo "Could not find file \"~cs/$SUBPATH\""
-	return 1
-}
-compdef "_directories -/ -W ~cs" cs-stuff
-
 
 #! Following commands use Windows Terminal, if you aren't using WT, remove the following
 # This command creates a new Powershell tab inside the current window of WT with the CWD as starting directory
