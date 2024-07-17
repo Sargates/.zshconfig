cbaddc178868ffa71ee4a5dce5eb9c079af7bdb5
Sargates <nick@WSL00-NG>
2024-07-17 16:39:40 -0500

    Fixed indentation in `install.bash`

--- install/install.bash
+++ install/install.bash
@@ -9,139 +9,139 @@ set -e
 
 #! Helper functions taken from OMZ's install script
 command_exists() {
-  command -v "$@" >/dev/null 2>&1
+	command -v "$@" >/dev/null 2>&1
 }
 
 if [ -t 1 ]; then
-  is_tty() {
-    true
-  }
+	is_tty() {
+		true
+	}
 else
-  is_tty() {
-    false
-  }
+	is_tty() {
+		false
+	}
 fi
 
 supports_hyperlinks() {
-  # $FORCE_HYPERLINK must be set and be non-zero (this acts as a logic bypass)
-  if [ -n "$FORCE_HYPERLINK" ]; then
-    [ "$FORCE_HYPERLINK" != 0 ]
-    return $?
-  fi
+	# $FORCE_HYPERLINK must be set and be non-zero (this acts as a logic bypass)
+	if [ -n "$FORCE_HYPERLINK" ]; then
+		[ "$FORCE_HYPERLINK" != 0 ]
+		return $?
+	fi
 
-  # If stdout is not a tty, it doesn't support hyperlinks
-  is_tty || return 1
+	# If stdout is not a tty, it doesn't support hyperlinks
+	is_tty || return 1
 
-  # DomTerm terminal emulator (domterm.org)
-  if [ -n "$DOMTERM" ]; then
-    return 0
-  fi
+	# DomTerm terminal emulator (domterm.org)
+	if [ -n "$DOMTERM" ]; then
+		return 0
+	fi
 
-  # VTE-based terminals above v0.50 (Gnome Terminal, Guake, ROXTerm, etc)
-  if [ -n "$VTE_VERSION" ]; then
-    [ $VTE_VERSION -ge 5000 ]
-    return $?
-  fi
+	# VTE-based terminals above v0.50 (Gnome Terminal, Guake, ROXTerm, etc)
+	if [ -n "$VTE_VERSION" ]; then
+		[ $VTE_VERSION -ge 5000 ]
+		return $?
+	fi
 
-  # If $TERM_PROGRAM is set, these terminals support hyperlinks
-  case "$TERM_PROGRAM" in
-  Hyper|iTerm.app|terminology|WezTerm|vscode) return 0 ;;
-  esac
+	# If $TERM_PROGRAM is set, these terminals support hyperlinks
+	case "$TERM_PROGRAM" in
+	Hyper|iTerm.app|terminology|WezTerm|vscode) return 0 ;;
+	esac
 
-  # These termcap entries support hyperlinks
-  case "$TERM" in
-  xterm-kitty|alacritty|alacritty-direct) return 0 ;;
-  esac
+	# These termcap entries support hyperlinks
+	case "$TERM" in
+	xterm-kitty|alacritty|alacritty-direct) return 0 ;;
+	esac
 
-  # xfce4-terminal supports hyperlinks
-  if [ "$COLORTERM" = "xfce4-terminal" ]; then
-    return 0
-  fi
+	# xfce4-terminal supports hyperlinks
+	if [ "$COLORTERM" = "xfce4-terminal" ]; then
+		return 0
+	fi
 
-  # Windows Terminal also supports hyperlinks
-  if [ -n "$WT_SESSION" ]; then
-    return 0
-  fi
+	# Windows Terminal also supports hyperlinks
+	if [ -n "$WT_SESSION" ]; then
+		return 0
+	fi
 
-  # Konsole supports hyperlinks, but it's an opt-in setting that can't be detected
-  # https://github.com/ohmyzsh/ohmyzsh/issues/10964
-  # if [ -n "$KONSOLE_VERSION" ]; then
-  #   return 0
-  # fi
+	# Konsole supports hyperlinks, but it's an opt-in setting that can't be detected
+	# https://github.com/ohmyzsh/ohmyzsh/issues/10964
+	# if [ -n "$KONSOLE_VERSION" ]; then
+	#	 return 0
+	# fi
 
-  return 1
+	return 1
 }
 
 # Adapted from code and information by Anton Kochkov (@XVilka)
 # Source: https://gist.github.com/XVilka/8346728
 supports_truecolor() {
-  case "$COLORTERM" in
-  truecolor|24bit) return 0 ;;
-  esac
+	case "$COLORTERM" in
+	truecolor|24bit) return 0 ;;
+	esac
 
-  case "$TERM" in
-  iterm           |\
-  tmux-truecolor  |\
-  linux-truecolor |\
-  xterm-truecolor |\
-  screen-truecolor) return 0 ;;
-  esac
+	case "$TERM" in
+	iterm           |\
+	tmux-truecolor  |\
+	linux-truecolor |\
+	xterm-truecolor |\
+	screen-truecolor) return 0 ;;
+	esac
 
-  return 1
+	return 1
 }
 
 fmt_link() {
-  # $1: text, $2: url, $3: fallback mode
-  if supports_hyperlinks; then
-    printf '\033]8;;%s\033\\%s\033]8;;\033\\\n' "$2" "$1"
-    return
-  fi
+	# $1: text, $2: url, $3: fallback mode
+	if supports_hyperlinks; then
+		printf '\033]8;;%s\033\\%s\033]8;;\033\\\n' "$2" "$1"
+		return
+	fi
 
-  case "$3" in
-  --text) printf '%s\n' "$1" ;;
-  --url|*) fmt_underline "$2" ;;
-  esac
+	case "$3" in
+	--text) printf '%s\n' "$1" ;;
+	--url|*) fmt_underline "$2" ;;
+	esac
 }
 
 fmt_underline() {
-  is_tty && printf '\033[4m%s\033[24m\n' "$*" || printf '%s\n' "$*"
+	is_tty && printf '\033[4m%s\033[24m\n' "$*" || printf '%s\n' "$*"
 }
 
 # shellcheck disable=SC2016 # backtick in single-quote
 fmt_code() {
-  is_tty && printf '`\033[2m%s\033[22m`\n' "$*" || printf '`%s`\n' "$*"
+	is_tty && printf '`\033[2m%s\033[22m`\n' "$*" || printf '`%s`\n' "$*"
 }
 
 fmt_error() {
-  printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
+	printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
 }
 
 fmt_warning() {
-  printf '%sWarning: %s%s\n' "${FMT_BOLD}${FMT_YELLOW}" "$*" "$FMT_RESET" >&2
+	printf '%sWarning: %s%s\n' "${FMT_BOLD}${FMT_YELLOW}" "$*" "$FMT_RESET" >&2
 }
 
 fmt_success() {
-  printf '%s%s%s\n' "${FMT_BOLD}${FMT_GREEN}" "$*" "$FMT_RESET" >&2
+	printf '%s%s%s\n' "${FMT_BOLD}${FMT_GREEN}" "$*" "$FMT_RESET" >&2
 }
 
 setup_color() {
-  # Only use colors if connected to a terminal
-  if ! is_tty; then
-    FMT_RED=""
-    FMT_GREEN=""
-    FMT_YELLOW=""
-    FMT_BLUE=""
-    FMT_BOLD=""
-    FMT_RESET=""
-    return
-  fi
+	# Only use colors if connected to a terminal
+	if ! is_tty; then
+		FMT_RED=""
+		FMT_GREEN=""
+		FMT_YELLOW=""
+		FMT_BLUE=""
+		FMT_BOLD=""
+		FMT_RESET=""
+		return
+	fi
 
-  FMT_RED=$(printf '\033[31m')
-  FMT_GREEN=$(printf '\033[32m')
-  FMT_YELLOW=$(printf '\033[33m')
-  FMT_BLUE=$(printf '\033[34m')
-  FMT_BOLD=$(printf '\033[1m')
-  FMT_RESET=$(printf '\033[0m')
+	FMT_RED=$(printf '\033[31m')
+	FMT_GREEN=$(printf '\033[32m')
+	FMT_YELLOW=$(printf '\033[33m')
+	FMT_BLUE=$(printf '\033[34m')
+	FMT_BOLD=$(printf '\033[1m')
+	FMT_RESET=$(printf '\033[0m')
 }
 
 
