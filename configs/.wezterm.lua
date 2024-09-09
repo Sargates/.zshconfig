8a5a4d6969821a10027a85f6fa94a852bb4fec7d
Sargates <nick@LUNA-NG>
2024-09-09 10:17:31 -0500

    Update `.wezterm.lua`

--- configs/.wezterm.lua
+++ configs/.wezterm.lua
@@ -11,15 +11,51 @@ config.color_scheme = 'Campbell (Gogh)'
 
 config.font_dirs = { '/home/nick/.local/share/fonts' }
 config.font_size = 10.0
+
 config.initial_rows = 30
 config.initial_cols = 112
+
+config.keys = {
+	{
+		key = 'W',
+		mods = 'CTRL|SHIFT',
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = 'w',
+		mods = 'CTRL',
+		action = wezterm.action.CloseCurrentPane { confirm = false },
+	},
+	{
+		key = 't',
+		mods = 'CTRL',
+		action = wezterm.action.SpawnTab "CurrentPaneDomain",
+	},
+	{
+		key = '=',
+		mods = 'ALT',
+		action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+	},
+	{
+		key = '-',
+		mods = 'ALT',
+		action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+	},
+	{
+		key = 'Tab',
+		mods = 'CTRL|SHIFT',
+		action = wezterm.action { ActivateTabRelative = -1 },
+	},
+
+	{
+		key = 'Tab',
+		mods = 'CTRL',
+		action = wezterm.action { ActivateTabRelative = 1 },
+	},
+}
+
+
 config.font = wezterm.font('FiraCode Nerd Font Mono')
-
-
---config.font = wezterm.font "Fira Code"
 config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 
---print(config.fonts_dir)
-
--- and finally, return the configuration to wezterm
 return config
