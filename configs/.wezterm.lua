84388b1d8c40eb22ffe456f0368845e1b4e338d8
nick@LUNA-NG <nick@LUNA-NG>
2024-11-08 14:15:36 -0600

    Updated wezterm config

--- configs/.wezterm.lua
+++ configs/.wezterm.lua
@@ -1,3 +1,5 @@
+--! Not for use in WSL. Won't function properly
+
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
@@ -7,13 +9,61 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Campbell (Gogh)'
+-- config.color_scheme = 'Campbell (Gogh)'
 
-config.font_dirs = { '/home/nick/.local/share/fonts' }
+
+-- config.font_dirs = { '/home/nick/.local/share/fonts' }
+
+config.font = wezterm.font('FiraCode Nerd Font Mono')
+config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 config.font_size = 10.0
 
 config.initial_rows = 30
 config.initial_cols = 112
+
+
+config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
+config.default_cursor_style = 'SteadyUnderline'
+config.cursor_thickness = '3px'
+
+config.scrollback_lines = 100000
+config.enable_scroll_bar = true
+
+config.colors = {
+	-- name = "Campbell",
+	foreground = "#CCCCCC",
+	background = "#000000",
+	cursor_bg = "#FFFFFF",
+	cursor_fg = "#000000",
+
+	selection_bg = "#FFFFFF",
+	selection_fg = '#000000',
+
+	scrollbar_thumb = '#222222',
+
+
+	ansi = {
+		"#000000", --black
+		"#C50F1F", --red
+		"#13A10E", --green
+		"#C19C00", --yellow
+		"#0037DA", --blue
+		"#881798", --purple
+		"#3A96DD", --cyan
+		"#CCCCCC", --white
+	},
+	brights = {
+		"#767676", -- brightBlack
+		"#E74856", -- brightRed
+		"#16C60C", -- brightGreen
+		"#F9F1A5", -- brightYellow
+		"#3B78FF", -- brightBlue
+		"#B4009E", -- brightPurple
+		"#61D6D6", -- brightCyan
+		"#F2F2F2", -- brightWhite
+	},
+}
+
 
 config.keys = {
 	{
@@ -54,8 +104,5 @@ config.keys = {
 	},
 }
 
-
-config.font = wezterm.font('FiraCode Nerd Font Mono')
-config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 
 return config
