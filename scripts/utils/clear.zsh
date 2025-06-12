6d2d25b9c785700501ccbd7c67c80daf02f88eb3
Sargates <nick@WSL00-NG>
2025-06-11 23:02:12 -0500

    Remove stupid clear function because I don't use tmux

--- scripts/utils/clear.zsh
+++ scripts/utils/clear.zsh
@@ -1,8 +1,8 @@
 # This function just prevents clearing the terminal in tmux sessions (in case theres important command output)
-clear() {
-	if (( ${+TMUX} )); then
-		/usr/bin/clear -x
-	else
-		/usr/bin/clear
-	fi	
-}
+# clear() {
+# 	if [[ -e /nix ]]; then
+# 		/run/current-system/sw/bin/clear
+# 	else
+# 		/usr/bin/clear
+# 	fi	
+# }
